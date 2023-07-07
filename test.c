607477bf34898a23ec61d7bbf645c06ef9ae9a4a
Alvin <alvinvaati@gmail.com>
2023-07-07 11:45:53 +0300

    you can ignore this file- nothing important

--- test.c
+++ test.c
@@ -1,76 +1,58 @@
 // #include <SDL2/SDL.h>
-
 // typedef int bool;
 // #define true 1
 // #define false 0
-
-// int main(int argc, char** argv)
-// {
-//     // variables
-//     (void)argc;
-//     (void)argv;
-//     bool quit = false;
-//     SDL_Event event;
-//     int x = 288;
-//     int y = 208;
-
-//     // init SDL
-
+// int main() {
 //     SDL_Init(SDL_INIT_VIDEO);
-//     SDL_Window* window = SDL_CreateWindow("SDL2 Keyboard/Mouse events",
-//                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-//     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-//     SDL_Surface* image = SDL_LoadBMP("pics/plane.bmp");
-//     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,
-//                                                         image);
-//     SDL_FreeSurface(image);
+//     // Create the first window and renderer
+//     SDL_Window* window1 = SDL_CreateWindow("Window 1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+//     SDL_Renderer* renderer1 = SDL_CreateRenderer(window1, -1, SDL_RENDERER_ACCELERATED);
 
-//     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+//     // Create the second window and renderer
+//     SDL_Window* window2 = SDL_CreateWindow("Window 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+//     SDL_Renderer* renderer2 = SDL_CreateRenderer(window2, -1, SDL_RENDERER_ACCELERATED);
 
-//     // handle events
+//     // Render something on window1
+//     SDL_SetRenderDrawColor(renderer1, 255, 0, 0, 255);
+//     SDL_RenderClear(renderer1);
+//     SDL_Rect rect1 = { 100, 100, 200, 150 };
+//     SDL_RenderFillRect(renderer1, &rect1);
+//     SDL_RenderPresent(renderer1);
 
-//     while (!quit)
-//     {
-//         SDL_PollEvent(&event);
+//     // Render something on window2
+//     SDL_SetRenderDrawColor(renderer2, 0, 0, 255, 255);
+//     SDL_RenderClear(renderer2);
+//     SDL_Rect rect2 = { 50, 50, 300, 200 };
+//     SDL_RenderFillRect(renderer2, &rect2);
+//     SDL_RenderPresent(renderer2);
 
-//         switch (event.type)
-//         {
-//         case SDL_QUIT:
-//             quit = true;
-//             break;
-//         case SDL_KEYDOWN:
-//             // Handle key down event
-//             switch (event.key.keysym.sym)
-//             {
-//             case SDLK_UP:
-//                 y -= 10;
-//                 break;
-//             case SDLK_DOWN:
-//                 y += 10;
-//                 break;
-//             case SDLK_LEFT:
-//                 x -= 10;
-//                 break;
-//             case SDLK_RIGHT:
-//                 x += 10;
-//                 break;
+//     SDL_Event event;
+//     bool quit = false;
+
+
+//     while (!quit) {
+//         while (SDL_PollEvent(&event)) {
+//             if (event.type == SDL_QUIT) {
+//                 if (event.window.windowID == SDL_GetWindowID(window1) || event.window.windowID == SDL_GetWindowID(window2)) {
+//                     quit = true;
+//                 }
 //             }
-//             break;
+//             else if (event.type == SDL_WINDOWEVENT) {
+//                 if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+//                     if (event.window.windowID == SDL_GetWindowID(window1) || event.window.windowID == SDL_GetWindowID(window2)) {
+//                         quit = true;
+//                     }
+//                 }
+//             }
 //         }
-
-//         SDL_Rect dstrect = { x, y, 64, 64 };
-
-//         SDL_RenderClear(renderer);
-//         SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-//         SDL_RenderPresent(renderer);
 //     }
 
-//     // cleanup SDL
-
-//     SDL_DestroyTexture(texture);
-//     SDL_DestroyRenderer(renderer);
-//     SDL_DestroyWindow(window);
+//     // Clean up resources
+//     SDL_DestroyRenderer(renderer1);
+//     SDL_DestroyWindow(window1);
+//     SDL_DestroyRenderer(renderer2);
+//     SDL_DestroyWindow(window2);
 //     SDL_Quit();
 
 //     return 0;
