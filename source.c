8b5ac85e72ea4d8b9ba0d92058cc399a19a6b756
Alvin <alvinvaati@gmail.com>
2023-07-07 11:44:10 +0300

    the main file

--- source.c
+++ source.c
@@ -5,41 +5,95 @@ int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    // Create a window and renderer
-    SDL_Window *window = SDL_CreateWindow("The Maze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowW, windowH, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
-    SDL_RenderClear(renderer);
+    createWindows();
+    // create player
+    Player *player = createPlayer();
+    int wallsCount = 0;
+    SDL_Point walls[wallmaxCount][2];
 
+    FILE *mapFile = fopen("map.txt", "r");
+    
+    if (mapFile == NULL)
+    {
+        SDL_Log("Failed to open map file: %s", SDL_GetError());
+        exit(1);
+    }
+    char line[256];
+
+    int i = 0;
+
+    char *token;
+    while (fgets(line, MAX_BUFFER_SIZE - 1, mapFile))
+    {
+        token = strtok(line, Separator);
+        walls[wallsCount][0].x = atoi(token);
+        token = strtok(NULL, Separator);
+        walls[wallsCount][0].y = atoi(token);
+        token = strtok(NULL, Separator);
+        walls[wallsCount][1].x = atoi(token);
+        token = strtok(NULL, Separator);
+        walls[wallsCount][1].y = atoi(token);
+        wallsCount++;
+    }
+    fclose(mapFile);
+    // Main loop
     SDL_Event event;
     int isRunning = 1;
-    // player spawn
-    Player player = createPlayer();
-    int pos[2] = {70, 70};
+
     while (isRunning)
-    {   
-        // handle events
+    {
+        // Handle events
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
             {
-                isRunning = 0;
+                if (event.window.windowID == SDL_GetWindowID(window) || event.window.windowID == SDL_GetWindowID(window2)) {
+                    isRunning = 0;
+                }
+            }else if (event.type == SDL_WINDOWEVENT) 
+            {
+                 if (event.window.event == SDL_WINDOWEVENT_CLOSE) 
+                 {
+                     if (event.window.windowID == SDL_GetWindowID(window2)) 
+                     {
+                         isRunning = 0;
+                     }
+                }
             }
-            // handle input
-            
-            }
+            eventHandler(&event, &isRunning, player, walls, wallsCount);
         }
-        // Draw a rectangle with the red color
-        wall(renderer, 650, 50, 650, 250);
-        drawPlayer(renderer, &player);
-        raycast(renderer, &player);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+        // Game logic and rendering
+        SDL_Texture* textureFloor = loadTexture(renderer2,"pics/greystone.png");
+        SDL_Texture* textureCeil = loadTexture(renderer2,"pics/wood.png");
+        //draw floor and ceiling
+        drawCeilFloor(renderer2,NULL,NULL);
+
+        // render player
+        drawPlayer(renderer, player);
+
+        // draw walls
+        drawWalls(renderer, walls, wallsCount);
+        drawRays(renderer,renderer2, player, walls, wallsCount);
+        drawPlayer(renderer, player);
+
+        SDL_Delay(1000 / 60);
+        // infiniteRay(renderer,player);
+        // raycast(renderer,player,wall1);
+        // SDL_RenderDrawLine(renderer, 50, 100, 100, 100);
+
+        // SDL_bool collision = SDL_HasIntersection(&rect1, &rect2);
+        // if (collision) {
+        //     SDL_RenderFillRect(renderer, &rect1);
+        // }else{
+        // SDL_RenderFillRect(renderer, &rect2);
+        // }
+        SDL_SetRenderDrawColor(renderer, COLOR_BLACK.r, COLOR_BLACK.g, COLOR_BLACK.b, COLOR_BLACK.a);
         // Update the renderer
-        sendFrame(renderer);
+        sendFrame(renderer,renderer2);
     }
+
     // Cleanup and exit
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    cleanUp();
     return 0;
 }
