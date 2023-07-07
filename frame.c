310930365339c9690c0bb4af2cd50970fdd8d3c1
Alvin <alvinvaati@gmail.com>
2023-07-07 11:36:31 +0300

    clears updates the renderer

--- frame.c
+++ frame.c
@@ -1,9 +1,11 @@
 #include "include/maze.h"
 #include "include/colors.h"
 
-void sendFrame(SDL_Renderer *renderer)
+void sendFrame(SDL_Renderer *renderer,SDL_Renderer *renderer2)
 {
     // Push the frame stored in the renderer to the screen
     SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer2);
     SDL_RenderClear(renderer);
+    SDL_RenderClear(renderer2);
 }
