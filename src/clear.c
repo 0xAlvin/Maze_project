93393eedcec07a572f750ee2a0788a0a2e2c013e
Alvin <alvinvaati@gmail.com>
2024-01-16 18:46:23 +0300

    Add clear.c and cleanUp functions

---
+++ src/clear.c
@@ -0,0 +1,27 @@
+#include "../include/maze.h"
+
+/**
+ * clear - clear screen
+ * @renderer: renderer
+ */
+
+void clear(SDL_Renderer *renderer)
+{
+SDL_SetRenderDrawColor(renderer, COLOR_GRAY.r, COLOR_GRAY.g,
+COLOR_GRAY.b, COLOR_GRAY.a);
+SDL_RenderPresent(renderer);
+SDL_RenderClear(renderer);
+}
+
+/**
+ * cleanUp - clean up
+ * @renderer: renderer
+ * @window: window
+*/
+
+void cleanUp(SDL_Renderer *renderer, SDL_Window *window)
+{
+SDL_DestroyRenderer(renderer);
+SDL_DestroyWindow(window);
+SDL_Quit();
+}
