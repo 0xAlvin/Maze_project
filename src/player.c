ee8c592faad4a39267f1bacf90999e8750c98997
Alvin <alvinvaati@gmail.com>
2024-01-16 20:49:38 +0300

    Fix player drawing position and add player direction line

--- src/player.c
+++ src/player.c
@@ -9,13 +9,18 @@ void drawPlayer(Player *player, SDL_Renderer *renderer)
 player->size.x = PLAYER_SIZE;
 player->size.y = PLAYER_SIZE;
 SDL_Rect playerRect = {
-(int)player->pos.x,
-(int)player->pos.y,
+(int)player->pos.x - (int)player->size.x / 2,
+(int)player->pos.y - (int)player->size.y / 2,
 (int)player->size.x,
 (int)player->size.y
 };
 SDL_SetRenderDrawColor(renderer, COLOR_GREEN.r, COLOR_GREEN.g,
 COLOR_GREEN.b, COLOR_GREEN.a);
 SDL_RenderFillRect(renderer, &playerRect);
-/*SDL_RenderPresent(renderer);*/
-}
+int linewidth = 3;
+for (int i = 0; i < linewidth; i++)
+{
+SDL_RenderDrawLine(renderer, player->pos.x, player->pos.y + i,
+player->pos.x + player->dir.x * 5,
+player->pos.y + player->dir.y * 5 + i);
+}}
