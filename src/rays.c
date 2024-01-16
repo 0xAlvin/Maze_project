dee5fc56f6623bd9532328f9480a5100527b566c
Alvin <alvinvaati@gmail.com>
2024-01-16 21:16:46 +0300

    Refactor drawRays function to improve ray tracing algorithm

--- src/rays.c
+++ src/rays.c
@@ -1,38 +1,55 @@
-#include"../include/maze.h"
+#include "../include/maze.h"
 /**
  * drawRays - draw rays
  * @player: player
  * @renderer: renderer
-*/
+ */
 void drawRays(Player *player, SDL_Renderer *renderer)
 {
-int r, mx, my, mp, rayLength;
-float rayX, rayY, rayAngle, xoffset, yoffset;
-rayAngle = player->Angle - FOV / 2;
-for (r = 0; r < NUM_RAYS; r++)
-{
-rayX = player->pos.x;
-rayY = player->pos.y;
-while (1)
-{
-mx = (int)rayX / TILE_SIZE;
-my = (int)rayY / TILE_SIZE;
-mp = (my *MAP_NUM_COLS) + mx;
-if (mp > 0 && mp < MAP_NUM_COLS * MAP_NUM_ROWS && map[mp] == 1)
-{
-break;
-}
-rayX += cos(rayAngle) * 5;
-rayY += sin(rayAngle) * 5;
-}
-SDL_SetRenderDrawColor(renderer, COLOR_RED.r, COLOR_RED.g,
-COLOR_RED.b, COLOR_RED.a);
-xoffset = rayX - player->pos.x;
-yoffset = rayY - player->pos.y;
-rayLength = sqrt(xoffset *xoffset + yoffset *yoffset);
-SDL_RenderDrawLine(renderer, player->pos.x, player->pos.y,
-player->pos.x + cos(rayAngle) * rayLength,
-player->pos.y + sin(rayAngle) * rayLength);
-rayAngle += FOV / NUM_RAYS;
-}
+    int r, mx, my, mp, rayLength;
+    float rayX, rayY, rayAngle, xoffset, yoffset;
+    rayAngle = player->Angle;
+    for (r = 0; r < NUM_RAYS; r++)
+    {
+        rayLength = 0;
+        float aTan = -1 / tan(rayAngle);
+        if (rayAngle > PI)
+        {
+            rayY = (((int)player->pos.y >> 6) << 6) - 0.0001;
+            rayX = (player->pos.y - rayY) * aTan + player->pos.x;
+            yoffset = -64;
+            xoffset = -yoffset * aTan;
+        }
+        if (rayAngle < PI)
+        {
+            rayY = (((int)player->pos.y >> 6) << 6) + 64;
+            rayX = (player->pos.y - rayY) * aTan + player->pos.x;
+            yoffset = 64;
+            xoffset = -yoffset * aTan;
+        }
+        if (rayAngle == 0 || rayAngle == PI)
+        {
+            rayX = player->pos.x;
+            rayY = player->pos.y;
+            rayLength = 8;
+        }
+        while (rayLength < 8)
+        {
+            mx = (int)(rayX) >> 6;
+            my = (int)(rayY) >> 6;
+            mp = my * mapX + mx;
+            int *mptr = map;
+            if (mp < mapX * mapY && mp >= 0 && mptr[mp] == 1)
+            {
+                rayLength = 8;
+            }
+            else
+            {
+                rayX += xoffset;
+                rayY += yoffset;
+                rayLength += 1;
+            }
+            drawLine(renderer, player->pos.x, player->pos.y, rayX, rayY);
+        }
+    }
 }
