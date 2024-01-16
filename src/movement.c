2cc98636336e9debfc05bd49132be54fbb804b97
Alvin <alvinvaati@gmail.com>
2024-01-16 20:49:34 +0300

    Refactor player movement and rotation logic

--- src/movement.c
+++ src/movement.c
@@ -9,16 +9,36 @@ void move(Player *player, SDL_Event keypress)
 switch (keypress.key.keysym.sym)
 {
 case SDLK_UP:
-player->pos.y -= PLAYER_SPEED;
+player->pos.x += player->dir.x * PLAYER_SPEED;
+player->pos.y += player->dir.y * PLAYER_SPEED;
 break;
 case SDLK_DOWN:
-player->pos.y += PLAYER_SPEED;
+player->pos.x -= player->dir.x * PLAYER_SPEED;
+player->pos.y -= player->dir.y * PLAYER_SPEED;
 break;
 case SDLK_LEFT:
 player->pos.x -= PLAYER_SPEED;
 break;
 case SDLK_RIGHT:
 player->pos.x += PLAYER_SPEED;
+break;
+case SDLK_a:
+player->Angle -= 0.1;
+if (player->Angle < 0)
+{
+player->Angle += 2 * PI;
+}
+player->dir.x = cos(player->Angle) * 5;
+player->dir.y = sin(player->Angle) * 5;
+break;
+case SDLK_d:
+player->Angle += 0.1;
+if (player->Angle > 2 * PI)
+{
+player->Angle -= 2 * PI;
+}
+player->dir.x = cos(player->Angle) * 5;
+player->dir.y = sin(player->Angle) * 5;
 break;
 default:
 break;
