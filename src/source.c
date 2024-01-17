340ba32bf21038fec54a95190e5fcc1fe6b210bd
Alvin <alvinvaati@gmail.com>
2024-01-17 12:20:27 +0300

    Refactor main function and improve code structure

--- src/source.c
+++ src/source.c
@@ -6,45 +6,44 @@
 
 int main(void)
 {
-init_();
-/*TTF_Font *font = TTF_OpenFont("../assets/font/font.ttf", 25);*/
-SDLResources *sdlResources = createWindow();
-Point spawn = {40, 40};
-Player *player = malloc(sizeof(Player));
-player->pos = spawn;
-player->dir.x = cos(player->Angle) * 5;
-player->dir.y = sin(player->Angle) * 5;
-/*Loop*/
-int running = true;
-SDL_Event event;
+    init_();
+    /*TTF_Font *font = TTF_OpenFont("../assets/font/font.ttf", 25);*/
+    SDLResources *sdlResources = createWindow();
+    Point spawn = {40, 40};
+    Player *player = malloc(sizeof(Player));
+    player->pos = spawn;
+    player->dir.x = cos(player->Angle) * 5;
+    player->dir.y = sin(player->Angle) * 5;
+    /*Loop*/
+    int running = true;
+    SDL_Event event;
 
-while (running)
-{
+    while (running)
+    {
 
-while (SDL_PollEvent(&event))
-{
-if (event.type == SDL_QUIT)
-{
-running = false;
-}
-if (event.type == SDL_KEYDOWN)
-{
-move(player, event);
-}
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+            if (event.type == SDL_KEYDOWN)
+            {
+                move(player, event);
+            }
 
-drawMap(sdlResources->renderer);
-drawRays(player, sdlResources->renderer);
-drawPlayer(player, sdlResources->renderer);
+            drawMap(sdlResources->renderer);
+            drawRays(player, sdlResources->renderer);
+            drawPlayer(player, sdlResources->renderer);
 
-/*Clear screen*/
-clear(sdlResources->renderer);
+            /*Clear screen*/
+            clear(sdlResources->renderer);
+        }
+    }
 
-}
-}
-
-/*On Exit*/
-free(player);
-cleanUp(sdlResources->renderer, sdlResources->window);
-// free(sdlResources);
-return (0);
+    /*On Exit*/
+    free(player);
+    cleanUp(sdlResources->renderer, sdlResources->window);
+    // free(sdlResources);
+    return (0);
 }
