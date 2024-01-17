18c436b5b54944efb812a47e3bfcca93e18fbc8d
Alvin <alvinvaati@gmail.com>
2024-01-17 16:03:17 +0300

    Refactor distance function to distanceBetweenPoints

--- src/dist.c
+++ src/dist.c
@@ -1,15 +1,13 @@
 #include"../include/maze.h"
 /**
- * distance - calculate distance
- * @ax - x coordinate
- * @ay - y coordinate
- * @bx - x coordinate
- * @by - y coordinate
- * @angle - angle
+ * distanceBetweenPoints - distance between two points
+ * @x1: x1
+ * @y1: y1
+ * @x2: x2
+ * @y2: y2
  * Return: distance
 */
-float distance(float ax, float ay, float bx, float by, float angle)
+float distanceBetweenPoints(float x1, float y1, float x2, float y2)
 {
-    printf("ax: %f, ay: %f, bx: %f, by: %f, angle: %f\n", ax, ay, bx, by, angle);
-    return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)));
+    return (sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
 }
