fd57f3b13a6def4ed2c287c8de1d0b1a4d550056
Alvin <alvinvaati@gmail.com>
2024-01-17 16:03:28 +0300

    Fix player angle range in movement.c

--- src/movement.c
+++ src/movement.c
@@ -25,32 +25,42 @@ void move(Player *player, SDL_Event keypress)
     if (keyboardState[SDL_SCANCODE_RIGHT])
     {
         player->Angle += PLAYER_TURN_SPEED;
+        if(player->Angle >= 2 * PI)
+        {
+            player->Angle -= 2 * PI; // Keep the angle within the range [0, 2*pi)
+        }
         player->dir.x = cos(player->Angle) * 5;
         player->dir.y = sin(player->Angle) * 5;
     }
     if (keyboardState[SDL_SCANCODE_LEFT])
     {
         player->Angle -= PLAYER_TURN_SPEED;
+        if(player->Angle < 0)
+        {
+            player->Angle += 2 * PI; // Keep the angle within the range [0, 2*pi)
+        }
         player->dir.x = cos(player->Angle) * 5;
         player->dir.y = sin(player->Angle) * 5;
     }
     if (keyboardState[SDL_SCANCODE_A])
     {
-        //based on the player angle, move the player left
+        // based on the player angle, move the player left
         player->pos.x -= player->dir.y * PLAYER_SPEED;
         player->pos.y += player->dir.x * PLAYER_SPEED;
-
-
     }
     if (keyboardState[SDL_SCANCODE_D])
     {
-        //based on the player angle, move the player right
+        // based on the player angle, move the player right
         player->pos.x += player->dir.y * PLAYER_SPEED;
         player->pos.y -= player->dir.x * PLAYER_SPEED;
     }
     if (keyboardState[SDL_SCANCODE_LEFT] && keyboardState[SDL_SCANCODE_UP])
     {
         player->Angle -= PLAYER_TURN_SPEED;
+        if (player->Angle < 0)
+        {
+            player->Angle += 2 * PI; // Keep the angle within the range [0, 2*pi)
+        }
         player->dir.x = cos(player->Angle) * 5;
         player->dir.y = sin(player->Angle) * 5;
         player->pos.x += player->dir.x * PLAYER_SPEED;
@@ -59,12 +69,17 @@ void move(Player *player, SDL_Event keypress)
     if (keyboardState[SDL_SCANCODE_RIGHT] && keyboardState[SDL_SCANCODE_UP])
     {
         player->Angle += PLAYER_TURN_SPEED;
+        if (player->Angle >= 2 * PI)
+        {
+            player->Angle -= 2 * PI; // Keep the angle within the range [0, 2*pi)
+        }
         player->dir.x = cos(player->Angle) * 5;
         player->dir.y = sin(player->Angle) * 5;
         player->pos.x += player->dir.x * PLAYER_SPEED;
         player->pos.y += player->dir.y * PLAYER_SPEED;
     }
-    if (keyboardState[SDL_SCANCODE_DOWN]){
+    if (keyboardState[SDL_SCANCODE_DOWN])
+    {
         player->pos.x -= player->dir.x * PLAYER_SPEED;
         player->pos.y -= player->dir.y * PLAYER_SPEED;
     }
