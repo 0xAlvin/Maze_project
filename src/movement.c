d2f4626886d6a8732c5a9e3179c2044b9f65b461
Alvin <alvinvaati@gmail.com>
2024-01-17 12:20:14 +0300

    Refactor movement logic in move function

--- src/movement.c
+++ src/movement.c
@@ -1,46 +1,71 @@
-#include"../include/maze.h"
+#include "../include/maze.h"
 /**
  * move - moves the player
  * @player: player
  * @keypress: keypress
-*/
+ */
 void move(Player *player, SDL_Event keypress)
 {
-switch (keypress.key.keysym.sym)
-{
-case SDLK_UP:
-player->pos.x += player->dir.x * PLAYER_SPEED;
-player->pos.y += player->dir.y * PLAYER_SPEED;
-break;
-case SDLK_DOWN:
-player->pos.x -= player->dir.x * PLAYER_SPEED;
-player->pos.y -= player->dir.y * PLAYER_SPEED;
-break;
-case SDLK_LEFT:
-player->pos.x -= PLAYER_SPEED;
-break;
-case SDLK_RIGHT:
-player->pos.x += PLAYER_SPEED;
-break;
-case SDLK_a:
-player->Angle -= 0.1;
-if (player->Angle < 0)
-{
-player->Angle += 2 * PI;
-}
-player->dir.x = cos(player->Angle) * 5;
-player->dir.y = sin(player->Angle) * 5;
-break;
-case SDLK_d:
-player->Angle += 0.1;
-if (player->Angle > 2 * PI)
-{
-player->Angle -= 2 * PI;
-}
-player->dir.x = cos(player->Angle) * 5;
-player->dir.y = sin(player->Angle) * 5;
-break;
-default:
-break;
-}
+    const Uint8 *keyboardState = SDL_GetKeyboardState(NULL);
+    if (keypress.type == SDL_QUIT)
+    {
+        // if it is quit the program
+        return;
+    }
+    if (keyboardState[SDL_SCANCODE_UP])
+    {
+        player->pos.x += player->dir.x * PLAYER_SPEED;
+        player->pos.y += player->dir.y * PLAYER_SPEED;
+    }
+    if (keyboardState[SDL_SCANCODE_DOWN])
+    {
+        player->pos.x -= player->dir.x * PLAYER_SPEED;
+        player->pos.y -= player->dir.y * PLAYER_SPEED;
+    }
+    if (keyboardState[SDL_SCANCODE_RIGHT])
+    {
+        player->Angle += PLAYER_TURN_SPEED;
+        player->dir.x = cos(player->Angle) * 5;
+        player->dir.y = sin(player->Angle) * 5;
+    }
+    if (keyboardState[SDL_SCANCODE_LEFT])
+    {
+        player->Angle -= PLAYER_TURN_SPEED;
+        player->dir.x = cos(player->Angle) * 5;
+        player->dir.y = sin(player->Angle) * 5;
+    }
+    if (keyboardState[SDL_SCANCODE_A])
+    {
+        //based on the player angle, move the player left
+        player->pos.x -= player->dir.y * PLAYER_SPEED;
+        player->pos.y += player->dir.x * PLAYER_SPEED;
+
+
+    }
+    if (keyboardState[SDL_SCANCODE_D])
+    {
+        //based on the player angle, move the player right
+        player->pos.x += player->dir.y * PLAYER_SPEED;
+        player->pos.y -= player->dir.x * PLAYER_SPEED;
+    }
+    if (keyboardState[SDL_SCANCODE_LEFT] && keyboardState[SDL_SCANCODE_UP])
+    {
+        player->Angle -= PLAYER_TURN_SPEED;
+        player->dir.x = cos(player->Angle) * 5;
+        player->dir.y = sin(player->Angle) * 5;
+        player->pos.x += player->dir.x * PLAYER_SPEED;
+        player->pos.y += player->dir.y * PLAYER_SPEED;
+    }
+    if (keyboardState[SDL_SCANCODE_RIGHT] && keyboardState[SDL_SCANCODE_UP])
+    {
+        player->Angle += PLAYER_TURN_SPEED;
+        player->dir.x = cos(player->Angle) * 5;
+        player->dir.y = sin(player->Angle) * 5;
+        player->pos.x += player->dir.x * PLAYER_SPEED;
+        player->pos.y += player->dir.y * PLAYER_SPEED;
+    }
+    if (keyboardState[SDL_SCANCODE_DOWN]){
+        player->pos.x -= player->dir.x * PLAYER_SPEED;
+        player->pos.y -= player->dir.y * PLAYER_SPEED;
+    }
 }
