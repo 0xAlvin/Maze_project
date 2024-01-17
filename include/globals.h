7caa51399e2188cc6cda25f6bdbb0f94c4180728
Alvin <alvinvaati@gmail.com>
2024-01-17 12:19:43 +0300

    Update map dimensions in globals.h

--- include/globals.h
+++ include/globals.h
@@ -5,7 +5,11 @@
 
 extern Point spawn;
 extern Player *player;
-extern int mapX, mapY, mapS;
-extern int map[];
+
+#define mapX 24
+#define mapY 24
+#define mapS 24*24
+
+extern int map[mapX][mapY];
 
 #endif /* __GLOBALS_H__ */
